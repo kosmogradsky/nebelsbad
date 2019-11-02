9ecb80f50a4ac95fcf6b5dc7b115b66c92b64935
Evgeny Kosmogradsky-Ivanov <kosmogradsky@gmail.com>
2019-11-02 13:54:21 +0100

    add basic linked list

---
+++ List.spec.ts
@@ -0,0 +1,100 @@
+import { List } from "./List";
+
+function* numbers(finishedCheck: () => void) {
+  let i = 0;
+  try {
+    while (true) {
+      yield ++i;
+    }
+  } finally {
+    finishedCheck();
+  }
+}
+
+function* numbersUpTo4(finishedCheck: () => void) {
+  let i = 0;
+  try {
+    while (true) {
+      const next = ++i;
+
+      yield next;
+
+      if (next === 4) {
+        return;
+      }
+    }
+  } finally {
+    finishedCheck();
+  }
+}
+
+describe("List", () => {
+  test("toArray method should work", () => {
+    const finishedCheck = jest.fn();
+
+    expect(new List(numbersUpTo4(finishedCheck)).toArray()).toEqual([
+      1,
+      2,
+      3,
+      4
+    ]);
+    expect(finishedCheck).toHaveBeenCalled();
+  });
+
+  test("take method should work", () => {
+    const finishedCheck = jest.fn();
+
+    expect(new List(numbers(finishedCheck)).take(4).toArray()).toEqual([
+      1,
+      2,
+      3,
+      4
+    ]);
+    expect(finishedCheck).toHaveBeenCalled();
+  });
+
+  test("should work with arrays", () => {
+    expect(new List([1, 2, 3, 4]).toArray()).toEqual([1, 2, 3, 4]);
+  });
+
+  test("should create singletons", () => {
+    expect(List.of(1).toArray()).toEqual([1]);
+  });
+
+  test("should create empty lists", () => {
+    expect(List.empty().toArray()).toEqual([]);
+  });
+
+  test("should concat", () => {
+    const finishedCheck = jest.fn();
+
+    expect(
+      List.concat(
+        numbersUpTo4(finishedCheck),
+        [5, 6],
+        List.of(7),
+        List.empty()
+      ).toArray()
+    ).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  });
+
+  test("should map", () => {
+    const finishedCheck = jest.fn();
+
+    expect(
+      new List(numbersUpTo4(finishedCheck)).map(num => num + 3).toArray()
+    ).toEqual([4, 5, 6, 7]);
+    expect(finishedCheck).toHaveBeenCalled();
+  });
+
+  test("should filter", () => {
+    const finishedCheck = jest.fn();
+
+    expect(
+      new List(numbersUpTo4(finishedCheck))
+        .filter(num => num % 2 === 0)
+        .toArray()
+    ).toEqual([2, 4]);
+    expect(finishedCheck).toHaveBeenCalled();
+  });
+});
---
+++ List.ts
@@ -0,0 +1,95 @@
+function* concat<T>(iterables: Iterable<Iterable<T>>): Generator<T> {
+  for (const iterable of iterables) {
+    for (const value of iterable) {
+      yield value;
+    }
+  }
+}
+
+function* empty<T>(): Generator<T> {
+  return;
+}
+
+function* of<T>(value: T): Generator<T> {
+  yield value;
+  return;
+}
+
+function* take<T>(count: number, source: Iterable<T>): Generator<T> {
+  let taken = 0;
+
+  for (const value of source) {
+    if (taken === count) {
+      return;
+    }
+
+    taken++;
+    yield value;
+  }
+}
+
+function* map<T, R>(
+  project: (value: T) => R,
+  source: Iterable<T>
+): Generator<R> {
+  for (const value of source) {
+    yield project(value);
+  }
+}
+
+function* filter<T>(
+  project: (value: T) => boolean,
+  source: Iterable<T>
+): Generator<T> {
+  for (const value of source) {
+    if (project(value)) {
+      yield value;
+    }
+  }
+}
+
+export class List<T> implements Iterable<T> {
+  constructor(private readonly iterable: Iterable<T>) {}
+
+  [Symbol.iterator]() {
+    return this.iterable[Symbol.iterator]();
+  }
+
+  static empty<T>(): List<T> {
+    return new List(empty<T>());
+  }
+
+  static of<T>(value: T): List<T> {
+    return new List(of(value));
+  }
+
+  static concat<T>(...iterables: Iterable<T>[]) {
+    return new List(concat(iterables));
+  }
+
+  static fromArray<T>(array: T[]): List<T> {
+    return new List(array[Symbol.iterator]());
+  }
+
+  take(count: number): List<T> {
+    return new List(take(count, this.iterable));
+  }
+
+  map<R>(project: (value: T) => R): List<R> {
+    return new List(map(project, this.iterable));
+  }
+
+  filter(project: (value: T) => boolean): List<T> {
+    return new List(filter(project, this.iterable));
+  }
+
+  toArray(): T[] {
+    const values: T[] = [];
+
+    for (const value of this.iterable) {
+      values.push(value);
+    }
+
+    return values;
+  }
+}
